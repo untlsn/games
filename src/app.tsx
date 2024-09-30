2a8acde6029121f12d2eb08063019857c9732544
Filip Skoczeń <philipskoczen@gmail.com>
2024-09-30 21:40:35 +0200

    Add reset and restart

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,21 @@
 import { createSelector, createSignal, For } from 'solid-js';
 import '~/assets/style';
 import clsx from 'clsx';
-import { createStore, produce } from 'solid-js/store';
+import { createStore, produce, reconcile } from 'solid-js/store';
 
 const colors = ['red', 'green', 'blue', 'orange', 'cyan', 'yellow'];
 
-export default function App() {
+function deepClone<T>(value: T): T {
+	return JSON.parse(JSON.stringify(value))
+}
+
+function createGrid() {
 	const availableColors = colors.flatMap(it => Array(4).fill(it) as string[])
 	const pickColor = () => {
 		return availableColors.splice(Math.floor(Math.random() * availableColors.length), 1)[0]
 	}
 
-  const [grids, setGrids] = createStore([
+	return [
 		[pickColor(), pickColor(), pickColor(), pickColor()],
 		[pickColor(), pickColor(), pickColor(), pickColor()],
 		[pickColor(), pickColor(), pickColor(), pickColor()],
@@ -20,13 +24,27 @@ export default function App() {
 		[pickColor(), pickColor(), pickColor(), pickColor()],
 		[],
 		[],
-	])
+	]
+}
+
+export default function App() {
+	const initialGrid = createGrid();
+
+  const [grids, setGrids] = createStore(deepClone(initialGrid))
 	const [selected, setSelected] = createSignal<number>();
 	const isSelected = createSelector(selected);
 
   return (
-    <main class="grid place-items-center min-h-screen bg-[#121212]">
-      <div class="grid-(~ cols-4) size-50vh">
+    <main class="grid place-items-center min-h-screen bg-[#121212] text-white">
+			<div class="flex gap-4">
+				<button onClick={() => setGrids(reconcile(deepClone(initialGrid)))}>
+					Reset
+				</button>
+				<button onClick={() => setGrids(reconcile(createGrid()))}>
+					Restart
+				</button>
+			</div>
+			<div class="grid-(~ cols-4) size-50vh">
 				<For each={grids} children={(fills, i) => (
 					<Bottle
 						fills={fills}
