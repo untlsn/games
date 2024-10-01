2fc5cc0681063bbcf75340aa52c79a4f0e70056f
Filip Skoczeń <philipskoczen@gmail.com>
2024-10-01 12:10:26 +0200

    Cleanup

--- src/app.tsx
+++ src/app.tsx
@@ -1,50 +1,49 @@
-import { createSelector, createSignal, For } from 'solid-js';
+import { createEffect, createSelector, createSignal, For } from 'solid-js';
 import '~/assets/style';
 import clsx from 'clsx';
 import { createStore, produce, reconcile } from 'solid-js/store';
-
-const colors = ['red', 'green', 'blue', 'orange', 'cyan', 'yellow'];
-
 function deepClone<T>(value: T): T {
 	return JSON.parse(JSON.stringify(value))
 }
 
-function createGrid() {
-	const availableColors = colors.flatMap(it => Array(4).fill(it) as string[])
-	const pickColor = () => {
-		return availableColors.splice(Math.floor(Math.random() * availableColors.length), 1)[0]
-	}
+function createGrid(colors: string[], empty: number): string[][] {
+	const availableColors = colors.flatMap(it => Array(4).fill(it) as string[]);
 
 	return [
-		[pickColor(), pickColor(), pickColor(), pickColor()],
-		[pickColor(), pickColor(), pickColor(), pickColor()],
-		[pickColor(), pickColor(), pickColor(), pickColor()],
-		[pickColor(), pickColor(), pickColor(), pickColor()],
-		[pickColor(), pickColor(), pickColor(), pickColor()],
-		[pickColor(), pickColor(), pickColor(), pickColor()],
-		[],
-		[],
+		...colors.map(() => {
+			return Array.from({ length: 4 }, () => {
+				return availableColors.splice(Math.floor(Math.random() * availableColors.length), 1)[0]
+			})
+		}),
+		...deepClone(Array(empty).fill([])),
 	]
 }
 
 export default function App() {
-	const initialGrid = createGrid();
+	const [colors, setColors] = createSignal<string[]>(['#FF0000', '#007F00', '#0000FF', '#FFA400', '#00FFFF', '#FFFF00']);
+	const [emptyCount, setEmptyCount] = createSignal(2);
+	const initialGrid = createGrid(colors(), emptyCount());
+
+	createEffect(() => {
+		console.log(colors());
+	})
 
   const [grids, setGrids] = createStore(deepClone(initialGrid))
 	const [selected, setSelected] = createSignal<number>();
 	const isSelected = createSelector(selected);
 
   return (
-    <main class="grid place-items-center min-h-screen bg-[#121212] text-white">
-			<div class="flex gap-4">
+    <main class="grid-(~ cols-1_2_1 rows-1_4) min-h-screen bg-[#121212] text-white">
+			<div class="flex gap-4 col-span-full mx-auto">
 				<button onClick={() => setGrids(reconcile(deepClone(initialGrid)))}>
 					Reset
 				</button>
-				<button onClick={() => setGrids(reconcile(createGrid()))}>
+				<button onClick={() => setGrids(reconcile(createGrid(colors(), emptyCount())))}>
 					Restart
 				</button>
 			</div>
-			<div class="grid-(~ cols-4) size-50vh">
+			<div />
+			<div class="grid-(~ cols-fit-20) gap-4 size-50vh mx-auto">
 				<For each={grids} children={(fills, i) => (
 					<Bottle
 						fills={fills}
@@ -76,7 +75,48 @@ export default function App() {
 					/>
 				)} />
 			</div>
-    </main>
+			<div>
+				<h2>Colors:</h2>
+				<ul class="list-decimal">
+					<For each={colors()} children={(it, i) => (
+						<li>
+							<input type="color" value={it} onChange={(ev) => {
+								const newColors = [...colors()]
+								newColors[i()] = ev.currentTarget.value
+								setColors(newColors)
+							}} />
+							<button
+								type="button"
+								class="i-ph-x mb-2"
+								onClick={() => {
+									setColors(colors().toSpliced(i(), 1))
+								}}
+							>
+								Remove
+							</button>
+						</li>
+					)}/>
+					<li class="list-none">
+						<button type="button" onClick={() => {
+							setColors([...colors(), '#ffffff'])
+						}}>
+							Add
+						</button>
+					</li>
+				</ul>
+				<div class="mt-4">
+					<p>Empty bottles:</p>
+					<input
+						type="number"
+						class="border-1 rounded-md px-2 py-1 mt-2"
+						value={emptyCount()}
+						onChange={ev => {
+							setEmptyCount(ev.currentTarget.valueAsNumber)
+						}}
+					/>
+				</div>
+			</div>
+		</main>
   );
 }
 
